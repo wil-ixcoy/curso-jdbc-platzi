cafc8e0a1285a9080d3d7e0d53ec1f3ce37a3bec
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2024-12-03 19:44:49 -0600

    se agrega interfaz de  java swing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,60 +4,15 @@ import org.example.model.EmployeeEntity;
 import org.example.repository.EmployeeRepository;
 import org.example.repository.Repository;
 import org.example.util.DatabaseConnection;
+import org.example.view.SwingApp;
 
 import java.sql.*;
 import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
+        SwingApp app = new SwingApp();
 
-
-        try (
-                Connection myConn = DatabaseConnection.getInstance();
-        ) {
-            System.out.println("Genial, nos conectamos");
-
-            Repository<EmployeeEntity> repository = new EmployeeRepository();
-
-            System.out.println("----------insertando-------------");
-           /*
-           *
-           *  EmployeeEntity employee = new EmployeeEntity();
-
-            employee.setFirst_name("Diego");
-            employee.setPa_surname("Pimentel");
-            employee.setMa_surname("Guiterrez");
-            employee.setEmail("diego@example.com");
-            employee.setSalary(23400F);
-
-            repository.save(employee);
-            *
-            * */
-
-
-            System.out.println("----------actualizando-------------");
-            EmployeeEntity employee = new EmployeeEntity();
-
-
-            employee.setMa_surname("Mendoza");
-            employee.setFirst_name("Juan");
-
-
-            repository.update(Optional.of(employee),8);
-
-
-            System.out.println("-----------------------");
-            System.out.println(repository.getById(8));
-
-
-            repository.delete(8);
-            System.out.println("--------Obtener todos-------");
-            repository.findAll().forEach(System.out::println);
-
-
-        } catch (SQLException e) {
-            System.out.println("Algo salió mal");
-            e.printStackTrace();
-        }
+        app.setVisible(true);
     }
 }
